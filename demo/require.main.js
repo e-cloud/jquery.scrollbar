3cfcb8ffa9befb38af63bca48ab062f75deea119
e-cloud <saintscott119@gmail.com>
2015-04-10 23:53:14 +0800

    1. fix angular demo little bug: see angular.html 2. add two demo: `angular-use-provider.html` and `requirejs.html` 3. add the new demo link to the `index.html`

---
+++ demo/require.main.js
@@ -0,0 +1,63 @@
+require.config({
+    paths: {
+        'jquery': '../includes/jquery',
+        'google-code-prettify': '../includes/prettify/prettify',
+        'jquery.scrollbar': '../jquery.scrollbar'
+    }
+})
+
+require(['jquery', 'google-code-prettify', 'jquery.scrollbar'], function (jQuery, prettify) {
+    jQuery(function ($) {
+
+        function getAlignedText(text) {
+            text = text.split('\n');
+            while (text.length > 0 && $.trim(text[0]) == '') {
+                text.shift();
+            }
+            var tabs = (text[0] || '').replace(/^(\s+).+$/, '$1');
+            for (var i = 0; i < text.length; i++) {
+                text[i] = text[i].replace(tabs, '');
+            }
+            if (text.length > 0 && text[text.length - 1].match(/^\s*$/)) {
+                text.pop();
+            }
+            return text.join('\n');
+        }
+
+        $('.container').each(function () {
+
+            var content = $(this).find('.content');
+            var controls = $(this).find('.controls');
+
+            console.log(1)
+            $('<pre></pre>').addClass('prettyprint linenums lang-js').text(getAlignedText(content.find('pre').html())).appendTo(content.find('.js'));
+            $('<pre></pre>').addClass('prettyprint linenums lang-html').text(getAlignedText(content.find('.demo').html())).appendTo(content.find('.html'));
+            $('<pre></pre>').addClass('prettyprint linenums lang-css').text(getAlignedText($('#css-common').html()) + "\n" + getAlignedText(content.find('style').html())).appendTo(content.find('.css'));
+
+
+            controls.on('click', 'span', function () {
+                content.find('.' + $(this).removeClass('active').attr('class')).show().siblings('div').hide();
+                $(this).addClass('active').siblings('span').removeClass('active');
+            });
+            controls.find('.demo').click();
+        });
+
+        $('.container').on('click', '.add-content', function () {
+            $('#lorem-ipsum').clone().removeAttr('id').appendTo($(this).closest('.container').find('.scroll-content'));
+            return false;
+        });
+        $('.container').on('click', '.remove-content', function () {
+            $(this).closest('.container').find('.scroll-content').find('p').not('.permanent').last().remove();
+            return false;
+        });
+
+        prettify.prettyPrint();
+        $('.wrapper').scrollbar();
+    });
+
+
+    jQuery(document).ready(function () {
+        jQuery('.scrollbar-inner').scrollbar();
+    });
+
+})
